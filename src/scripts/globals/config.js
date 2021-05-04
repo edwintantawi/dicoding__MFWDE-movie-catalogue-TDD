990b3a4ddf5afdf91846081da533edb5d337c2e5
edwintantawi <edwintantawi@gmail.com>
2021-05-04 12:09:08 +0700

    unActive websocket and push notification

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '3912a06fb200b29e4e15a494a4d6e733',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
